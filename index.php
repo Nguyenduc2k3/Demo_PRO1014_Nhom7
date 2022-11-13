d0ccf97d98e356f48eef5c7dafad2b90d7a70367
Lyzhv03 <106502221+Lyzhv03@users.noreply.github.com>
2022-11-13 17:55:58 +0700

    lý code giao diện admin

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php 
 
-// Đây là index
-<<<<<<< HEAD
-// text align center
-=======
-// Phần của lyhvph
->>>>>>> a5cbcfe142457ed6f26488271f61675631b240b9
+?>
--- views/admin/manage/users.php
+++ views/admin/manage/users.php
@@ -6,18 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./public/admin/css/style.css">
+    <link rel="stylesheet" href="/public/admin/css/style.css">
+    <link rel="stylesheet" href="../views/admin/block/style.css">
     <link rel="stylesheet" href="style.css">
+    
 </head>
 <body>
     <div class="container">
     <header>
-        <div class="logo"> <img src="logo.png" alt=""></div>
+        <div class="logo"> <img src="/views/admin/block/logo.png" alt=""></div>
     </header>
     <main>
         <div class="nav">
             <ul>
-                <li><a href="/views/admin/manage/users.php">Quản lí người dùng</a></li>
+                <li><a href="">Quản lí người dùng</a></li>
                 <li><a href="">Danh sách sản phẩm</a></li>
                 <li><a href="">Thêm sản phẩm</a></li>
                 <li><a href="">Bình luận</a></li>
@@ -25,7 +27,9 @@
             </ul>
         </div>
         <div class="section">
-        <p class="test"> Chào Mừng Bạn Quay Trở Lại Admin! </p>
+        <table>
+
+        </table>
         </div>
     </main>
     <footer></footer>
