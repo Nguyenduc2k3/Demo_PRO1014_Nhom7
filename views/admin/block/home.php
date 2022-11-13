789218837a2944e526109e5b71e8753171d9cb4e
Lyzhv03 <106502221+Lyzhv03@users.noreply.github.com>
2022-11-13 10:58:12 +0700

    admin

--- views/admin/block/home.php
+++ views/admin/block/home.php
@@ -6,11 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/public/admin/css/index.css">
 </head>
 <body>
-    <header>ADMIN</header>
-    <main></main>
+    <div class="container">
+    <header>
+        <div class="logo"></div>
+    </header>
+    <main>
+        <div class="nav">
+            <ul>
+                <li><a href="">Quản lí người dùng</a></li>
+                <li><a href="">Danh sách sản phẩm</a></li>
+                <li><a href="">Thêm sản phẩm</a></li>
+                <li><a href="">Bình luận</a></li>
+                <li><a href="">Thống kê</a></li>
+            </ul>
+        </div>
+        <div class="section">
+
+        </div>
+    </main>
     <footer></footer>
+    </div>
 </body>
 </html>
 ?>
