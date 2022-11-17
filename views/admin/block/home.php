ae164cedd2b60d3dc92c0467f2af8a36494b7ec5
Lyzhv03 <106502221+Lyzhv03@users.noreply.github.com>
2022-11-17 17:41:19 +0700

    Trang login admin

--- views/admin/block/home.php
+++ views/admin/block/home.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./public/admin/css/style.css">
+    <!-- <link rel="stylesheet" href="http::/localhost/Demo_PRO1014_Nhom7/public/admin/css/style.css"> -->
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -19,9 +19,11 @@
             <ul>
                 <li><a href="/views/admin/manage/users.php">Quản lí người dùng</a></li>
                 <li><a href="">Danh sách sản phẩm</a></li>
+                <li><a href="">Quản lí lịch đặt hàng</a></li>
                 <li><a href="">Thêm sản phẩm</a></li>
                 <li><a href="">Bình luận</a></li>
                 <li><a href="">Thống kê</a></li>
+                <li><a href="login.php">Thoát</a></li>
             </ul>
         </div>
         <div class="section">
---
+++ views/admin/block/login.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="login">
+                <div class="logo">
+                    <img src="logo.png" alt="">
+                </div>
+                <div class="form">
+                    <form action="" method="get">
+                        <input type="text" placeholder="Enter username..">
+                        <input type="password" placeholder="Enter password..">
+                        <span><input type="checkbox"> <label for="">Nhớ mật khẩu</label></span>
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
